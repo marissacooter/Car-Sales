554a027c7479cc1933d668bddd658379d3a83273
Marissa <marissacooterl@gmail.com>
2020-04-14 17:06:47 -0500

    got web page to remove features properly and update the price

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -21,7 +21,7 @@ const reducer = (state = initalState, action) => {
             return {...state, car: {...state.car, features: state.car.features.concat(action.payload)}, additionalPrice: state.additionalPrice + action.payload.price}
 
         case 'REMOVE_FEATURE':
-            return state.filter(feature => !feature.completed)
+            return {...state, car: {...state.car, features: state.car.features.filter(feature => feature.id !== action.payload.id)}, additionalPrice: state.additionalPrice - action.payload.price}
         default:
             return state;
     }
