61da3ab66f078855f1034efd35a5fd4911048b7e
Marissa <marissacooterl@gmail.com>
2020-04-14 15:57:32 -0500

    created index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'; // bridge piece
+import {reducer} from './reducers/reducer';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(reducer)
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    rootElement
+  );
 
